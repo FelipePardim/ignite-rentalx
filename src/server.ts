0681b49366706accecb3bfe80ac8924be7c7519e
Felipe Pardim <felipesantos.2017@alunos.utfpr.edu.br>
2022-02-17 18:40:51 -0300

    Adding create course mock route

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3333, () => {
   console.log("Server Running!");
+
+  app.post("/courses", (request, response) => {
+    const { name } = request.body;
+    return response.json({ name });
+  });
 });
