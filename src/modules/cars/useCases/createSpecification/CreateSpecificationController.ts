30f4081e73122f151f22efbf60f58458919e3e37
FelipePardim <felipesantos.2017@alunos.utfpr.edu.br>
2022-03-25 16:37:15 -0300

    Fixing create specification routes

--- src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationController.ts
@@ -4,7 +4,6 @@ import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
 
 class CreateSpecificationController {
   constructor(private createSpecificationUseCase: CreateSpecificationUseCase) {}
-
   handle(request: Request, response: Response): Response {
     const { name, description } = request.body;
     this.createSpecificationUseCase.execute({ name, description });
